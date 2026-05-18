73688986bb37362d273a117ac5ce68597a4b2d83
Yue Sun <yuesun@g.harvard.edu>
2026-05-18 15:01:01 -0400

    Formatting and refactoring

--- src/prob/prob_init_fluid_usr.cpp
+++ src/prob/prob_init_fluid_usr.cpp
@@ -23,28 +23,23 @@ void incflo::init_cryo_plunging (amrex::Box const& vbx, amrex::Box const& /*gbx*
         Real y = (j+0.5)*dx[1] - 0.5*(probhi[1] - problo[1]);
         Real z = (k+0.5)*dx[2] -     (probhi[2] - problo[2]);
 
-        // Set initial density and velocity fields
-        density(i,j,k) = Real(1.0);
-        vel(i,j,k,0)   = Real(0.0);
-        vel(i,j,k,1)   = Real(0.0);
-        vel(i,j,k,2)   = Real(0.0);
-
-        // Set initial tracer, temperature, and heat fields
-        // TODO
-
-        // Set initial cell_type
-        cell_type(i,j,k) = Real(-1.0);
-
-        // Update velocity and cell_type for each cell
-        // based on the prescribed velocity of the plunging protocol
         Real &velx = vel(i,j,k,0);
         Real &vely = vel(i,j,k,1);
         Real &velz = vel(i,j,k,2);
         Real &cell_type_ijk = cell_type(i,j,k);
-    #ifdef INCFLO_SIM_CRYO
-        cryo_update(i, j, k, x, y, z, velx, vely, velz, cell_type_ijk,
-                m_cur_time, dx, problo, probhi);
-    #endif
+
+        // Initialize density, velocity, and cell_type for each cell
+        density(i,j,k) = Real(1.0);
+        velx = vely = velz = Real(0.0);
+        cell_type_ijk = Real(-1.0);
+
+        // Update velocity and cell_type for each cell
+        // based on the prescribed velocity of the plunging protocol
+#ifdef INCFLO_SIM_CRYO
+        cryo_set_geom_velocity(i, j, k, x, y, z,
+                            velx, vely, velz, cell_type_ijk,
+                            m_cur_time, dx, problo, probhi);
+#endif
 
         // Update thermal properties for each cell
         // -1: liquid ethane (fluid)
