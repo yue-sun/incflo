f36a5860cde108ee4b13aa7f689c8441cb54ebf0
Yue Sun <yuesun@g.harvard.edu>
2026-05-18 14:59:22 -0400

    Fix small dt and instantiate arrays for prescribed plunging protocols

--- src/incflo_compute_dt.cpp
+++ src/incflo_compute_dt.cpp
@@ -22,7 +22,7 @@ using namespace amrex;
 //
 // WARNING: We use a slightly modified version of C in the implementation below
 //
-void incflo::ComputeDt (int initialization, bool explicit_diffusion)
+void incflo::ComputeDt(int initialization, bool explicit_diffusion)
 {
     BL_PROFILE("incflo::ComputeDt");
 
@@ -37,55 +37,55 @@ void incflo::ComputeDt (int initialization, bool explicit_diffusion)
     for (int lev = 0; lev <= finest_level; ++lev)
     {
         auto const dxinv = geom[lev].InvCellSizeArray();
-        MultiFab const& vel   = m_leveldata[lev]->velocity;
-        MultiFab const& rho   = m_leveldata[lev]->density;
-        MultiFab const& tra   = m_leveldata[lev]->tracer;
-        MultiFab const& tra_o = m_leveldata[lev]->tracer_o;
+        MultiFab const &vel = m_leveldata[lev]->velocity;
+        MultiFab const &rho = m_leveldata[lev]->density;
+        MultiFab const &tra = m_leveldata[lev]->tracer;
+        MultiFab const &tra_o = m_leveldata[lev]->tracer_o;
 
         Real conv_lev = Real(0.0);
         Real diff_lev = Real(0.0);
         Real forc_lev = Real(0.0);
 
-       // Make a temporary here to hold vel_forces
-       MultiFab vel_forces(grids[lev], dmap[lev], AMREX_SPACEDIM, 0);
+        // Make a temporary here to hold vel_forces
+        MultiFab vel_forces(grids[lev], dmap[lev], AMREX_SPACEDIM, 0);
 
-       compute_vel_forces_on_level (lev, vel_forces, vel, rho, tra_o, tra);
+        compute_vel_forces_on_level(lev, vel_forces, vel, rho, tra_o, tra);
 
 #ifdef AMREX_USE_EB
-        if (!vel.isAllRegular()) {
-            auto const& flag = EBFactory(lev).getMultiEBCellFlagFab();
+        if (!vel.isAllRegular())
+        {
+            auto const &flag = EBFactory(lev).getMultiEBCellFlagFab();
             conv_lev = amrex::ReduceMax(vel, flag, 0,
-                       [=] AMREX_GPU_HOST_DEVICE (Box const& b,
-                                                  Array4<Real const> const& v,
-                                                  Array4<EBCellFlag const> const& f) -> Real
-                       {
-                           Real mx = -1.0;
-                           amrex::Loop(b, [=,&mx] (int i, int j, int k) noexcept
-                           {
+                                        [=] AMREX_GPU_HOST_DEVICE(Box const &b,
+                                                                  Array4<Real const> const &v,
+                                                                  Array4<EBCellFlag const> const &f) -> Real
+                                        {
+                                            Real mx = -1.0;
+                                            amrex::Loop(b, [=, &mx](int i, int j, int k) noexcept
+                                                        {
                                if (!f(i,j,k).isCovered()) {
                                    mx = amrex::max(AMREX_D_DECL(amrex::Math::abs(v(i,j,k,0))*dxinv[0],
                                                                 amrex::Math::abs(v(i,j,k,1))*dxinv[1],
                                                                 amrex::Math::abs(v(i,j,k,2))*dxinv[2]), mx);
-                               }
-                           });
-                           return mx;
-                       });
-            if (explicit_diffusion) {
+                               } });
+                                            return mx;
+                                        });
+            if (explicit_diffusion)
+            {
                 diff_lev = amrex::ReduceMax(rho, flag, 0,
-                           [=] AMREX_GPU_HOST_DEVICE (Box const& b,
-                                                      Array4<Real const> const& r,
-                                                      Array4<EBCellFlag const> const& f) -> Real
-                          {
-                              Real mx = Real(-1.0);
-                              amrex::Loop(b, [=,&mx] (int i, int j, int k) noexcept
-                              {
+                                            [=] AMREX_GPU_HOST_DEVICE(Box const &b,
+                                                                      Array4<Real const> const &r,
+                                                                      Array4<EBCellFlag const> const &f) -> Real
+                                            {
+                                                Real mx = Real(-1.0);
+                                                amrex::Loop(b, [=, &mx](int i, int j, int k) noexcept
+                                                            {
                                   if (!f(i,j,k).isCovered()) {
                                       Real rho_inv = Real(1.0)/r(i,j,k);
                                       mx = amrex::max(rho_inv, mx);
-                                  }
-                              });
-                              return mx;
-                          });
+                                  } });
+                                                return mx;
+                                            });
                 diff_lev *= m_mu;
             }
 
@@ -97,51 +97,50 @@ void incflo::ComputeDt (int initialization, bool explicit_diffusion)
 
             // Forcing term -- new way of computing using "actual" forcing term
             forc_lev = amrex::ReduceMax(vel_forces, flag, 0,
-                  [=] AMREX_GPU_HOST_DEVICE (Box const& b,
-                                             Array4<Real const> const& vf,
-                                             Array4<EBCellFlag const> const& f) -> Real
-                  {
-                      Real mx = Real(-1.0);
-                      amrex::Loop(b, [=,&mx] (int i, int j, int k) noexcept
-                      {
+                                        [=] AMREX_GPU_HOST_DEVICE(Box const &b,
+                                                                  Array4<Real const> const &vf,
+                                                                  Array4<EBCellFlag const> const &f) -> Real
+                                        {
+                                            Real mx = Real(-1.0);
+                                            amrex::Loop(b, [=, &mx](int i, int j, int k) noexcept
+                                                        {
                           if (!f(i,j,k).isCovered()) {
                               mx = amrex::max(AMREX_D_DECL(amrex::Math::abs(vf(i,j,k,0))*dxinv[0],
                                                            amrex::Math::abs(vf(i,j,k,1))*dxinv[1],
                                                            amrex::Math::abs(vf(i,j,k,2))*dxinv[2]), mx);
-                          }
-                      });
-                      return mx;
-                  });
-        } else
+                          } });
+                                            return mx;
+                                        });
+        }
+        else
 #endif
         {
             conv_lev = amrex::ReduceMax(vel, 0,
-                       [=] AMREX_GPU_HOST_DEVICE (Box const& b,
-                                                  Array4<Real const> const& v) -> Real
-                       {
-                           Real mx = Real(-1.0);
-                           amrex::Loop(b, [=,&mx] (int i, int j, int k) noexcept
-                           {
-                               mx = amrex::max(AMREX_D_DECL(amrex::Math::abs(v(i,j,k,0))*dxinv[0],
-                                                            amrex::Math::abs(v(i,j,k,1))*dxinv[1],
-                                                            amrex::Math::abs(v(i,j,k,2))*dxinv[2]), mx);
-                           });
-                           return mx;
-                       });
+                                        [=] AMREX_GPU_HOST_DEVICE(Box const &b,
+                                                                  Array4<Real const> const &v) -> Real
+                                        {
+                                            Real mx = Real(-1.0);
+                                            amrex::Loop(b, [=, &mx](int i, int j, int k) noexcept
+                                                        { mx = amrex::max(AMREX_D_DECL(amrex::Math::abs(v(i, j, k, 0)) * dxinv[0],
+                                                                                       amrex::Math::abs(v(i, j, k, 1)) * dxinv[1],
+                                                                                       amrex::Math::abs(v(i, j, k, 2)) * dxinv[2]),
+                                                                          mx); });
+                                            return mx;
+                                        });
 
-            if (explicit_diffusion) {
+            if (explicit_diffusion)
+            {
                 diff_lev = amrex::ReduceMax(rho, 0,
-                           [=] AMREX_GPU_HOST_DEVICE (Box const& b,
-                                                      Array4<Real const> const& r) -> Real
-                           {
-                               Real mx = Real(-1.0);
-                               amrex::Loop(b, [=,&mx] (int i, int j, int k) noexcept
-                               {
+                                            [=] AMREX_GPU_HOST_DEVICE(Box const &b,
+                                                                      Array4<Real const> const &r) -> Real
+                                            {
+                                                Real mx = Real(-1.0);
+                                                amrex::Loop(b, [=, &mx](int i, int j, int k) noexcept
+                                                            {
                                    Real rho_inv = Real(1.0)/r(i,j,k);
-                                   mx = amrex::max(rho_inv, mx);
-                               });
-                               return mx;
-                           });
+                                   mx = amrex::max(rho_inv, mx); });
+                                                return mx;
+                                            });
                 diff_lev *= m_mu;
             }
 
@@ -153,38 +152,40 @@ void incflo::ComputeDt (int initialization, bool explicit_diffusion)
 
             // Forcing term -- new way of computing using "actual" forcing term
             forc_lev = amrex::ReduceMax(vel_forces, 0,
-                  [=] AMREX_GPU_HOST_DEVICE (Box const& b,
-                                             Array4<Real const> const& vf) -> Real
-                  {
-                      Real mx = Real(-1.0);
-                      amrex::Loop(b, [=,&mx] (int i, int j, int k) noexcept
-                      {
-                          mx = amrex::max(AMREX_D_DECL(amrex::Math::abs(vf(i,j,k,0))*dxinv[0],
-                                                       amrex::Math::abs(vf(i,j,k,1))*dxinv[1],
-                                                       amrex::Math::abs(vf(i,j,k,2))*dxinv[2]), mx);
-                      });
-                      return mx;
-                  });
+                                        [=] AMREX_GPU_HOST_DEVICE(Box const &b,
+                                                                  Array4<Real const> const &vf) -> Real
+                                        {
+                                            Real mx = Real(-1.0);
+                                            amrex::Loop(b, [=, &mx](int i, int j, int k) noexcept
+                                                        { mx = amrex::max(AMREX_D_DECL(amrex::Math::abs(vf(i, j, k, 0)) * dxinv[0],
+                                                                                       amrex::Math::abs(vf(i, j, k, 1)) * dxinv[1],
+                                                                                       amrex::Math::abs(vf(i, j, k, 2)) * dxinv[2]),
+                                                                          mx); });
+                                            return mx;
+                                        });
         }
 
         forc_cfl = std::max(forc_cfl, forc_lev);
         conv_cfl = std::max(conv_cfl, conv_lev);
 
 #if (AMREX_SPACEDIM == 2)
-        Real dxinv_norm = dxinv[0]*dxinv[0]+dxinv[1]*dxinv[1];
+        Real dxinv_norm = dxinv[0] * dxinv[0] + dxinv[1] * dxinv[1];
 #else
-        Real dxinv_norm = dxinv[0]*dxinv[0]+dxinv[1]*dxinv[1]+dxinv[2]*dxinv[2];
+        Real dxinv_norm = dxinv[0] * dxinv[0] + dxinv[1] * dxinv[1] + dxinv[2] * dxinv[2];
 #endif
 
-        diff_cfl = std::max(diff_cfl, diff_lev*Real(2.0)*dxinv_norm);
+        diff_cfl = std::max(diff_cfl, diff_lev * Real(2.0) * dxinv_norm);
     }
 
     Real cd_cfl;
-    if (explicit_diffusion) {
-        ParallelAllReduce::Max<Real>({conv_cfl,diff_cfl},
+    if (explicit_diffusion)
+    {
+        ParallelAllReduce::Max<Real>({conv_cfl, diff_cfl},
                                      ParallelContext::CommunicatorSub());
         cd_cfl = conv_cfl + diff_cfl;
-    } else {
+    }
+    else
+    {
         ParallelAllReduce::Max<Real>(conv_cfl,
                                      ParallelContext::CommunicatorSub());
         cd_cfl = conv_cfl;
@@ -194,28 +195,29 @@ void incflo::ComputeDt (int initialization, bool explicit_diffusion)
                                  ParallelContext::CommunicatorSub());
 
     // Combined CFL conditioner
-    Real comb_cfl = cd_cfl + std::sqrt(cd_cfl*cd_cfl + Real(4.0) * forc_cfl);
+    Real comb_cfl = cd_cfl + std::sqrt(cd_cfl * cd_cfl + Real(4.0) * forc_cfl);
 
     // Update dt
     Real dt_new;
     if (comb_cfl > 0.)
     {
         dt_new = Real(2.0) * m_cfl / comb_cfl;
-
-    } else {
+    }
+    else
+    {
 
         // This is totally random but just a way to set a timestep
         // when the initial velocity is zero and the forcing term
         // is not a body force
-        auto const dx    = geom[finest_level].CellSizeArray();
-        dt_new = std::min(dx[0],dx[1]);
+        auto const dx = geom[finest_level].CellSizeArray();
+        dt_new = std::min(dx[0], dx[1]);
 #if (AMREX_SPACEDIM == 3)
-        dt_new = std::min(dt_new,dx[2]);
+        dt_new = std::min(dt_new, dx[2]);
 #endif
     }
 
     // Optionally reduce CFL for initial step
-    if(initialization)
+    if (initialization)
     {
         dt_new *= m_init_shrink;
     }
@@ -224,34 +226,47 @@ void incflo::ComputeDt (int initialization, bool explicit_diffusion)
     // This may happen, for example, when the initial velocity field
     // is zero for an inviscid flow with no external forcing
     Real eps = std::numeric_limits<Real>::epsilon();
-    if(! initialization && comb_cfl <= eps)
+    if (!initialization && comb_cfl <= eps)
     {
         dt_new = Real(0.5) * m_dt;
+        if (m_sim_cryo)
+        {
+            dt_new = amrex::max(dt_new, m_dt_min);
+            amrex::Print() << "WARNING: comb_cfl is very small (" << comb_cfl << "). Setting dt to " << dt_new << std::endl;
+        }
     }
 
     // Don't let the timestep grow by more than m_dt_change_max per step
     // unless the previous time step was unduly shrunk to match m_plot_per_exact
     Real allowed_change_factor = m_dt_change_max;
-    if( (m_dt > Real(0.0)) && !(m_plot_per_exact > 0 && m_last_plt == m_nstep && m_nstep > 0) )
+    if ((m_dt > Real(0.0)) && !(m_plot_per_exact > 0 && m_last_plt == m_nstep && m_nstep > 0))
     {
         dt_new = amrex::min(dt_new, allowed_change_factor * m_prev_dt);
     }
-    else if ( (m_dt > Real(0.0)) && (m_plot_per_exact > 0 && m_last_plt == m_nstep && m_nstep > 0) )
+    else if ((m_dt > Real(0.0)) && (m_plot_per_exact > 0 && m_last_plt == m_nstep && m_nstep > 0))
     {
-        dt_new = amrex::min( dt_new, allowed_change_factor * amrex::max(m_prev_dt, m_prev_prev_dt) );
+        dt_new = amrex::min(dt_new, allowed_change_factor * amrex::max(m_prev_dt, m_prev_prev_dt));
     }
 
-    // Don't overshoot specified plot times
-    if(m_plot_per_exact > Real(0.0) &&
-            (std::trunc((m_cur_time + dt_new + eps) / m_plot_per_exact) > std::trunc((m_cur_time + eps) / m_plot_per_exact)))
+    // Don't overshoot specified plot times, but avoid tiny dt
+    if (m_plot_per_exact > Real(0.0) &&
+        (std::trunc((m_cur_time + dt_new + eps) / m_plot_per_exact) > std::trunc((m_cur_time + eps) / m_plot_per_exact)))
     {
-        dt_new = std::trunc((m_cur_time + dt_new) / m_plot_per_exact) * m_plot_per_exact - m_cur_time;
+        Real dt_plot = std::trunc((m_cur_time + dt_new) / m_plot_per_exact) * m_plot_per_exact - m_cur_time;
+        // Enforce a minimum dt (m_dt_min if set, else 10x machine epsilon)
+        Real min_dt = (m_dt_min > 0.0) ? m_dt_min : 10.0 * eps;
+        if (dt_plot < min_dt) {
+            amrex::Print() << "WARNING: dt_new to match plot_per_exact would be too small (" << dt_plot << "). Using min_dt = " << min_dt << std::endl;
+            dt_new = min_dt;
+        } else {
+            dt_new = dt_plot;
+        }
     }
 
     // Don't overshoot the final time if not running to steady state
-    if(!m_steady_state && m_stop_time > Real(0.0))
+    if (!m_steady_state && m_stop_time > Real(0.0))
     {
-        if(m_cur_time + dt_new > m_stop_time)
+        if (m_cur_time + dt_new > m_stop_time)
         {
             dt_new = m_stop_time - m_cur_time;
         }
@@ -266,16 +281,16 @@ void incflo::ComputeDt (int initialization, bool explicit_diffusion)
     // If using fixed time step, check CFL condition and give warning if not satisfied
     if (m_fixed_dt > Real(0.0))
     {
-    if(dt_new < m_fixed_dt)
-    {
-        amrex::Print() << "WARNING: fixed_dt does not satisfy CFL condition: \n"
-                       << "max dt by CFL     : " << dt_new << "\n"
-                       << "fixed dt specified: " << m_fixed_dt << std::endl;
-    }
-    m_dt = m_fixed_dt;
+        if (dt_new < m_fixed_dt)
+        {
+            amrex::Print() << "WARNING: fixed_dt does not satisfy CFL condition: \n"
+                           << "max dt by CFL     : " << dt_new << "\n"
+                           << "fixed dt specified: " << m_fixed_dt << std::endl;
+        }
+        m_dt = m_fixed_dt;
     }
     else
     {
-    m_dt = dt_new;
+        m_dt = dt_new;
     }
 }
