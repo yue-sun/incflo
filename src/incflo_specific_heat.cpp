782a5649492e7180263c9ae263acfe89ad6ce1be
Yue Sun <yuesun@g.harvard.edu>
2026-05-29 10:04:07 -0400

    Update cp based on cell_type

--- src/incflo_specific_heat.cpp
+++ src/incflo_specific_heat.cpp
@@ -2,16 +2,42 @@
 
 using namespace amrex;
 
-void incflo::compute_cp (int /*lev*/, MFIter& /*mfi*/, FArrayBox& cp) const
+void incflo::compute_cp (int lev, MFIter& mfi, FArrayBox& cp) const
 {
-    // Get leveldata if desired, e.g.
-    // Array4<Real const> const& rho   = m_leveldata[lev]->density.const_array(mfi);
-
     Box const& bx = cp.box();
     Array4<Real> const& cp_a = cp.array();
-    Real l_cp = m_cp;
+    bool const conservative_temperature = !m_iconserv_temperature.empty() && m_iconserv_temperature[0] == 1;
+
+    if (!conservative_temperature && !m_sim_cryo) {
+        Real l_cp = m_cp;
+        ParallelFor(bx, [=] AMREX_GPU_DEVICE (int i, int j, int k) noexcept
+        {
+            cp_a(i,j,k) = l_cp;
+        });
+        return;
+    }
+
+#ifdef INCFLO_SIM_CRYO
+    auto const& ct = m_leveldata[lev]->cell_type.const_array(mfi);
     ParallelFor(bx, [=] AMREX_GPU_DEVICE (int i, int j, int k) noexcept
     {
-        cp_a(i,j,k) = l_cp;
+        if (ct(i,j,k) == -2) {
+            cp_a(i,j,k) = cp_tcp;
+        } else if (ct(i,j,k) == -3) {
+            cp_a(i,j,k) = cp_plu;
+        } else if (ct(i,j,k) == -4) {
+            cp_a(i,j,k) = cp_sap;
+        } else if (ct(i,j,k) == -5) {
+            cp_a(i,j,k) = cp_dia;
+        } else if (ct(i,j,k) == -6) {
+            cp_a(i,j,k) = cp_plu;
+        } else if (ct(i,j,k) == -1) {
+            cp_a(i,j,k) = cp_eth;
+        } else if (ct(i,j,k) >= 0) {
+            cp_a(i,j,k) = cp_sam;
+        } else {
+            cp_a(i,j,k) = m_cp;
+        }
     });
+#endif
 }
